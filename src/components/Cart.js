9b2368af9d8172a78dcfada7ab25a464792270e6
Ignacio Manfredini <nachomanfredini@gmail.com>
2024-03-05 21:32:43 -0300

    problemas con el fondo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import CartItem from "./CartItem";
 import { Link } from "react-router-dom";
+import "../styles/components/Cart.css";
 
 function Cart(){
 
     const {cart, addProduct, clearCart, removeItem} = useContext(CartContext);
 
     return (
-        <div>
+        <div className="Cart">
             {cart.length == 0 ? <h1>There are no products in cart</h1> : cart.map(prod => <CartItem product={prod}/>)} 
             <button className="boton">
                 <Link to="/">
